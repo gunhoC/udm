e3c36d4a442508f63c8454e83df7f25a53d563a0
Choi <vnthf2@ajou.ac.kr>
2019-01-22 14:10:31 +0900

    udm

--- RecyclerViewAdapter.java
+++ RecyclerViewAdapter.java
@@ -7,7 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup; //
 import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.TextView;////
 
 import java.util.List;
 
